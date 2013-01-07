0adf5827524f00fab82c05baed277489951ddddf
Jonathan <jonathan@traceoutdoor.fr>
2013-01-07 22:41:05 +0100

    Update investors JSON mock with required fields

--- src/app.php
+++ src/app.php
@@ -9,13 +9,12 @@ $app->get('/api/{city}/investors', function () use ($app) {
               array(
                   'name' => 'Grenoble Angels',
                   'address' => 'CCI de Grenoble, 1 Place André Malraux BP 297, 38016, Grenoble',
+                  'logo' => '',
                   'lat' => '45.188529',
                   'lng' => '5.724524',
                   'email' => 'grenobleangels@grenoble.cci.fr',
                   'website' => 'http://www.grenoble-ecobiz.biz/ccig/gaacc.nsf/EXMHP?ReadForm',
-                  'twitter' => array(),
-                  'facebook' => array(),
-                  'linkedin' => array()
+                  'twitter' => array()
               )
           );
           
