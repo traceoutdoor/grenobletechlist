9f5d995f3533f93a4a7709ed52adb224ea0c6dae
Jonathan <jonathan@traceoutdoor.fr>
2013-01-11 00:38:22 +0100

    Use Dropbox as CDN for companies images

--- src/app.php
+++ src/app.php
@@ -9,7 +9,7 @@ $app->get('/api/{city}/startups', function () use ($app) {
             array(
                   'name' => 'Sogilis',
                   'address' => '1, place Firmin Gautier 38000 Grenoble',
-                  'logo' => 'https://secure.gravatar.com/avatar/b727aa9f321a45573e04bcda0187cfce?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/bw_aps0BtN/images/sogilis.png',
                   'lat' => '45.19079',
                   'lng' => '5.71206',
                   'email' => 'contact@sogilis.com',
@@ -19,7 +19,7 @@ $app->get('/api/{city}/startups', function () use ($app) {
             array(
                   'name' => 'Wizbii',
                   'address' => '6 rue Etienne Marcel, 38000 Grenoble',
-                  'logo' => 'https://si0.twimg.com/profile_images/643550211/Logo_Wizbii_qualit_e_moyenne_blanc_centr_.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/cBYu2aZcHl/images/wizbii.jpg',
                   'lat' => '45.18641',
                   'lng' => '5.71758',
                   'email' => '',
@@ -29,7 +29,7 @@ $app->get('/api/{city}/startups', function () use ($app) {
             array(
                   'name' => 'Ethera',
                   'address' => '7, Parvis Louis Néel, BP 50, F-38040 Grenoble Cedex 9',
-                  'logo' => 'https://si0.twimg.com/profile_images/1119710971/Picto_Avatar.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/utWe7pESAm/images/ethera.jpg',
                   'lat' => '45.19574',
                   'lng' => '5.71005',
                   'email' => 'info@ethera-labs.com',
@@ -48,7 +48,7 @@ $app->get('/api/{city}/investors', function () use ($app) {
               array(
                   'name' => 'Grenoble Angels',
                   'address' => 'CCI de Grenoble, 1 Place André Malraux BP 297, 38016, Grenoble',
-                  'logo' => 'http://www.semaineba.com/v2008/img/logo_grenoble_angels.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/bbZ8aG3qUS/images/grenoble-angels.jpg',
                   'lat' => '45.188529',
                   'lng' => '5.724524',
                   'email' => 'grenobleangels@grenoble.cci.fr',
@@ -58,7 +58,7 @@ $app->get('/api/{city}/investors', function () use ($app) {
               array(
                   'name' => 'Réseau Entreprendre Isère',
                   'address' => 'ZAC Bouchayer-Viallet, Hôtel d\'entreprises de la petite halle, 31 rue Gustave Eiffel, 38000 Grenoble',
-                  'logo' => 'http://www.reseau-entreprendre-isere.fr/images/200x78/association/id_0/logos_sites/isere.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/BOM-RUNfsF/images/reseau-entreprendre-isere.jpg',
                   'lat' => '45.18440',
                   'lng' => '5.70342',
                   'email' => '',
@@ -68,7 +68,7 @@ $app->get('/api/{city}/investors', function () use ($app) {
               array(
                   'name' => 'ADIE',
                   'address' => '17, rue Denfert Rochereau 38000 Grenoble',
-                  'logo' => 'https://si0.twimg.com/profile_images/3086691165/7e2b12fae46d4edc5187e246642f60ae.png',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/SlCTsejUGs/images/adie.png',
                   'lat' => '45.18920',
                   'lng' => '5.71727',
                   'email' => '',
@@ -78,7 +78,7 @@ $app->get('/api/{city}/investors', function () use ($app) {
               array(
                   'name' => 'MCAE',
                   'address' => '35, rue Casimir Brenier 38000 Grenoble',
-                  'logo' => 'http://www.alpesolidaires.org/files/imagecache/pictogram/MCAE_isere_active_9.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/BsTSe0_waM/images/mcae.jpg',
                   'lat' => '45.19146',
                   'lng' => '5.71654',
                   'email' => '',
@@ -97,7 +97,7 @@ $app->get('/api/{city}/incubators', function () use ($app) {
             array(
                   'name' => 'GRAIN',
                   'address' => 'Petite Halle – Site Bouchayer-Viallet, 31 rue Gustave Eiffel, 38000 Grenoble',
-                  'logo' => 'https://twimg0-a.akamaihd.net/profile_images/2843792622/0beab5ef951431607f770f9ba04e8d36.png',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/JJPVXasqkL/images/grain.png',
                   'lat' => '45.18440',
                   'lng' => '5.70342',
                   'email' => '',
@@ -107,7 +107,7 @@ $app->get('/api/{city}/incubators', function () use ($app) {
               array(
                   'name' => 'IncubaGEM',
                   'address' => '4 place Robert Schuman, 38000 Grenoble',
-                  'logo' => 'http://gementreprendre.com/wp-content/uploads/2012/01/logo-incubagem.png',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/af9kDtNfVu/images/incubagem.png',
                   'lat' => '45.19162',
                   'lng' => '5.71298',
                   'email' => '',
@@ -117,7 +117,7 @@ $app->get('/api/{city}/incubators', function () use ($app) {
               array(
                   'name' => 'ACEISP',
                   'address' => 'Espace Vaucanson, 82 rue Anatole France, 38100 Grenoble',
-                  'logo' => 'http://www.rhone-solidaires.org/files/imagecache/pictogram/Aceisp_2.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/AN4I7dpone/images/aceisp.jpg',
                   'lat' => '45.17285',
                   'lng' => '5.70370',
                   'email' => '',
@@ -127,7 +127,7 @@ $app->get('/api/{city}/incubators', function () use ($app) {
               array(
                   'name' => 'Petale',
                   'address' => 'La Petite Halle, 31 Rue Gustave Eiffel, 38 000 Grenoble',
-                  'logo' => 'http://www.lametro.fr/uploads/Image/9f/IMF_MOBILE_VIGNETTE_WEB_CHEMIN_641_1250065755.jpg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/0FO2gDbaYy/images/petale.jpg',
                   'lat' => '45.18440',
                   'lng' => '5.70342',
                   'email' => '',
@@ -144,9 +144,9 @@ $app->get('/api/{city}/incubators', function () use ($app) {
 $app->get('/api/{city}/coworking-spaces', function () use ($app) {
           $coworkingSpaces = array(
             array(
-                  'name' => 'Col\'inn',
+                  'name' => "Col'inn",
                   'address' => '34, avenue Felix Viallet, 38000 Grenoble',
-                  'logo' => 'https://si0.twimg.com/profile_images/2813570537/9936a4db9f29471ff19c45515f9f3472.jpeg',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/yLaS8UOx0x/images/colinn.jpeg',
                   'lat' => '45.19084',
                   'lng' => '5.71874',
                   'website' => 'http://www.col-inn.net/',
@@ -164,7 +164,7 @@ $app->get('/api/{city}/other-organizations', function () use ($app) {
             array(
                   'name' => 'Apéro Entrepreneurs',
                   'address' => '4 Rue Montorge, 38000 Grenoble',
-                  'logo' => 'https://si0.twimg.com/profile_images/1227212489/logo_carr_.png',
+                  'logo' => 'https://dl.dropbox.com/sh/a8jxbpxbckf2naa/imai264oBa/images/apero-entrepreneurs.png',
                   'lat' => '45.19139',
                   'lng' => '5.72712',
                   'website' => 'http://aperoentrepreneurs.com/grenoble/show/',
